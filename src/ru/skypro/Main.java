d9ac01875d0221e481452402c3336553b4e6556f
Евгений <ugend84@yandex.ru>
2022-07-22 16:11:04 +0300

    Коммит

--- src/ru/skypro/Main.java
+++ src/ru/skypro/Main.java
@@ -1,8 +1,11 @@
 package ru.skypro;
 
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        List<String> names = List.of("John","Mary");
+        names.stream().map(e->e.substring(1)).sorted().toList().stream().forEach(System.out::println);
     }
 }
